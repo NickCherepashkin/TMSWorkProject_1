d12097a1500b35164e13531d79357749509f230c
Nika Drozdova <cherepawka13@mail.ru>
2022-10-30 23:50:46 +0300

    add task1, task2, task3

--- lib/src/main/java/com/drozdova/tms/lib/HomeWork3.java
+++ lib/src/main/java/com/drozdova/tms/lib/HomeWork3.java
@@ -1,5 +1,49 @@
 package com.drozdova.tms.lib;
 
+import java.util.Scanner;
+
 public class HomeWork3 {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        for (int i = 1; i < 14; i++) {
+            System.out.print(i * 7 + " ");
+        }
+    }
+
+    public static void task2() {
+        Scanner scan = new Scanner(System.in);
+        while (!scan.hasNextInt()) {
+            scan.next();
+            System.out.println("Введены некорректные данные. Попробуйте снова.");
+        }
+        int num = scan.nextInt();
+        if (num == 7) {
+            System.out.println("Ура, вы выиграли айфон!");
+        }
+    }
+
+    public static void task3() {
+        String str = "I am Android developer";
+        for (char item: str.toCharArray()) {
+            System.out.println(item);
+        }
+    }
     
+    public static void task4() {
+
+    }
+    public static void task5() {
+
+    }
+    public static void task6() {
+
+    }
+    public static void task7() {
+
+    }
 }
